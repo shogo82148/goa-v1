94b730ad88a45e0c2dfd1cb5c6292748e3aed1a8
Ichinose Shogo <shogo82148@gmail.com>
2020-06-30 23:57:13 +0900

    add script for releasing

---
+++ release.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -eux
+
+CURRENT=$(cd "$(dirname "$0")" && pwd)
+VERSION=$1
+MAJOR=$(echo "$VERSION" | cut -d. -f1)
+MINOR=$(echo "$VERSION" | cut -d. -f2)
+PATCH=$(echo "$VERSION" | cut -d. -f3)
+
+cd "$CURRENT"
+git switch master
+
+cat <<EOF > version/version.go
+package version
+
+const (
+	// Major version number
+	Major = ${MAJOR}
+	// Minor version number
+	Minor = ${MINOR}
+	// Build version number
+	Build = ${PATCH}
+)
+EOF
+perl -i -pe "s(Current Release: \`v[.0-9]+\`)(Current Release: \`v$VERSION\`)" README.md
+git add version/version.go README.md
+git commit -m "bump up v$VERSION"
+git tag "v$VERSION"
+git push origin master
+git push --tags
